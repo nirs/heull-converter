ac008c2da57b23c88570d405daed612790eb2c90
Nir Soffer <nsoffer@redhat.com>
2019-10-18 23:02:48 +0300

    More cleanup
    - Move time conversion helpers to new time module
    - Keep internal time in seconds and display unit

--- app/src/main/java/com/huellbabineaux/huellconverter/MainActivity.kt
+++ app/src/main/java/com/huellbabineaux/huellconverter/MainActivity.kt
@@ -8,67 +8,47 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
-
-const val HOURS_PER_HUELL = 1.71666667
-
-interface Time {
-    fun hours() : Double
-    fun minutes() : Double
-    fun seconds() : Double
-    fun huells() : Double
-}
-
-enum class Units : Time {
-    HOURS {
-        override fun hours() : Double = 1.0
-        override fun minutes() : Double = 60.0
-        override fun seconds() : Double = 3600.0
-        override fun huells() : Double = hours() / HOURS_PER_HUELL
-    },
-    MINUTES {
-        override fun hours() : Double = 1.0 / 60.0
-        override fun minutes() : Double = 1.0
-        override fun seconds() : Double = 60.0
-        override fun huells() : Double = hours() / HOURS_PER_HUELL
-    },
-    SECONDS {
-        override fun hours() : Double = 1.0 / 3600.0
-        override fun minutes() : Double = 1.0 / 60.0
-        override fun seconds() : Double = 1.0
-        override fun huells() : Double = hours() / HOURS_PER_HUELL
-    }
-}
+import com.huellbabineaux.huellconverter.time.secondsFrom
+import com.huellbabineaux.huellconverter.time.secondsTo
+import com.huellbabineaux.huellconverter.time.Units
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var huells: EditText
-    private lateinit var hours : EditText
-    private lateinit var label : TextView
+    private lateinit var huellsText: EditText
+    private lateinit var intervalText : EditText
+    private lateinit var intervalLabel : TextView
 
-    private var currentUnit = Units.HOURS
+    private var displayUnit = Units.HOURS
+    private var seconds = secondsFrom(1.0, Units.HUELLS)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        huells = findViewById(R.id.huells_text)
-        hours = findViewById(R.id.hours_text)
-        label = findViewById(R.id.hours_label)
+        huellsText = findViewById(R.id.huells_text)
+        intervalText = findViewById(R.id.interval_text)
+        intervalLabel = findViewById(R.id.interval_label)
 
-        huells.addTextChangedListener(object : Watcher() {
+        huellsText.addTextChangedListener(object : Watcher() {
             override fun afterTextChanged(e: Editable?) {
-                if (currentFocus == huells) updateHours()
+                if (currentFocus == huellsText) {
+                    changeSeconds(e.toString(), Units.HUELLS)
+                    updateIntervalText()
+                }
             }
         })
 
-        hours.addTextChangedListener(object : Watcher() {
+        intervalText.addTextChangedListener(object : Watcher() {
             override fun afterTextChanged(e: Editable?) {
-                if (currentFocus == hours) updateHuells()
+                if (currentFocus == intervalText) {
+                    changeSeconds(e.toString(), displayUnit)
+                    updateHuellsText()
+                }
             }
         })
 
-        hours.requestFocus()
-        hours.setText(formatTime(HOURS_PER_HUELL))
+        intervalText.requestFocus()
+        updateIntervalText()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -79,57 +59,40 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.hours -> {
-                changeUnit(Units.HOURS)
+                changeDisplayUnit(Units.HOURS)
                 true
             }
             R.id.minutes -> {
-                changeUnit(Units.MINUTES)
+                changeDisplayUnit(Units.MINUTES)
                 true
             }
             R.id.seconds -> {
-                changeUnit(Units.SECONDS)
+                changeDisplayUnit(Units.SECONDS)
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    fun changeUnit(newUnit: Units) {
-        val text = hours.text.toString()
-        val oldUnit = currentUnit
+    fun changeDisplayUnit(unit: Units) {
+        displayUnit = unit
+        intervalLabel.setText(unit.toString())
+        updateIntervalText()
+    }
 
-        currentUnit = newUnit
-        label.setText(newUnit.toString())
-
-        if (!text.isEmpty()) {
-            val currentValue = text.toDouble()
-            val newValue = when (newUnit) {
-                Units.HOURS -> {currentValue * oldUnit.hours()}
-                Units.MINUTES -> {currentValue * oldUnit.minutes()}
-                Units.SECONDS -> {currentValue * oldUnit.seconds()}
-            }
-            hours.setText(formatTime(newValue))
+    fun changeSeconds(text: String, unit: Units) {
+        seconds = when (text) {
+            "" -> 0.0
+            else -> secondsFrom(text.toDouble(), unit)
         }
     }
 
-    fun updateHuells() {
-        val text = hours.text.toString()
-        if (text.isEmpty())
-            huells.setText("")
-        else {
-            val newValue = text.toDouble() * currentUnit.huells()
-            huells.setText(formatTime(newValue))
-        }
+    fun updateIntervalText() {
+        intervalText.setText(formatTime(secondsTo(seconds, displayUnit)))
     }
 
-    fun updateHours() {
-        val text = huells.text.toString()
-        if (text == "")
-            hours.setText("")
-        else {
-            val newValue = text.toDouble() / currentUnit.huells()
-            hours.setText(formatTime(newValue))
-        }
+    fun updateHuellsText() {
+        huellsText.setText(formatTime(secondsTo(seconds, Units.HUELLS)))
     }
 
     fun formatTime(value : Double) : String {
---
+++ app/src/main/java/com/huellbabineaux/huellconverter/Time.kt
@@ -0,0 +1,21 @@
+package com.huellbabineaux.huellconverter.time
+
+const val HOURS_PER_HUELL = 1.71666667
+
+enum class Units {
+    HOURS, MINUTES, SECONDS, HUELLS
+}
+
+fun secondsFrom(value: Double, unit: Units) = when(unit) {
+    Units.HUELLS -> value * HOURS_PER_HUELL * 3600
+    Units.HOURS -> value * 3600
+    Units.MINUTES -> value * 60
+    Units.SECONDS -> value
+}
+
+fun secondsTo(seconds: Double, unit: Units) : Double = when(unit) {
+    Units.HOURS -> seconds / 3600
+    Units.MINUTES -> seconds / 60
+    Units.SECONDS -> seconds
+    Units.HUELLS -> seconds / 3600 / HOURS_PER_HUELL
+}
